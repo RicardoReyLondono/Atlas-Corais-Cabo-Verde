f17906455f7e65a12af570402a07a50c4d8505bc
RRL <100435711+RicardoReyLondono@users.noreply.github.com>
2024-10-14 12:16:26 +0100

    Sao Vicente

---
+++ Data/PontosCorais.js
@@ -0,0 +1,248 @@
+var PontosCorais =
+{
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "id": 1,
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -24.993344443999945,
+          16.89775555600005
+        ]
+      },
+      "properties": {
+        "OBJECTID": 1,
+        "Name": "Matiota",
+        "Especie_Coral": "Siderastrea radians",
+        "AltMode": -1,
+        "Base": 0,
+        "Snippet": "",
+        "PopupInfo": "",
+        "HasLabel": -1,
+        "LabelID": 0
+      }
+    },
+    {
+      "type": "Feature",
+      "id": 2,
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -24.93816666699996,
+          16.91026944400005
+        ]
+      },
+      "properties": {
+        "OBJECTID": 2,
+        "Name": "Salamansa",
+        "Especie_Coral": null,
+        "AltMode": -1,
+        "Base": 0,
+        "Snippet": "",
+        "PopupInfo": "",
+        "HasLabel": -1,
+        "LabelID": 0
+      }
+    },
+    {
+      "type": "Feature",
+      "id": 3,
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -24.905166666999946,
+          16.90116388900003
+        ]
+      },
+      "properties": {
+        "OBJECTID": 3,
+        "Name": "Baía das Gatas",
+        "Especie_Coral": null,
+        "AltMode": -1,
+        "Base": 0,
+        "Snippet": "",
+        "PopupInfo": "",
+        "HasLabel": -1,
+        "LabelID": 0
+      }
+    },
+    {
+      "type": "Feature",
+      "id": 4,
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -24.867313888999945,
+          16.86518055600004
+        ]
+      },
+      "properties": {
+        "OBJECTID": 4,
+        "Name": "Praia Grande",
+        "Especie_Coral": null,
+        "AltMode": -1,
+        "Base": 0,
+        "Snippet": "",
+        "PopupInfo": "",
+        "HasLabel": -1,
+        "LabelID": 0
+      }
+    },
+    {
+      "type": "Feature",
+      "id": 5,
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -24.861772221999956,
+          16.851277778000053
+        ]
+      },
+      "properties": {
+        "OBJECTID": 5,
+        "Name": "Calhau",
+        "Especie_Coral": null,
+        "AltMode": -1,
+        "Base": 0,
+        "Snippet": "",
+        "PopupInfo": "",
+        "HasLabel": -1,
+        "LabelID": 0
+      }
+    },
+    {
+      "type": "Feature",
+      "id": 6,
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -24.858502777999945,
+          16.83689722200006
+        ]
+      },
+      "properties": {
+        "OBJECTID": 6,
+        "Name": "Ponta de Viana",
+        "Especie_Coral": null,
+        "AltMode": -1,
+        "Base": 0,
+        "Snippet": "",
+        "PopupInfo": "",
+        "HasLabel": -1,
+        "LabelID": 0
+      }
+    },
+    {
+      "type": "Feature",
+      "id": 7,
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -24.864980555999978,
+          16.81065555600003
+        ]
+      },
+      "properties": {
+        "OBJECTID": 7,
+        "Name": "Tupim",
+        "Especie_Coral": null,
+        "AltMode": -1,
+        "Base": 0,
+        "Snippet": "",
+        "PopupInfo": "",
+        "HasLabel": -1,
+        "LabelID": 0
+      }
+    },
+    {
+      "type": "Feature",
+      "id": 8,
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -24.88254722199997,
+          16.810063889000048
+        ]
+      },
+      "properties": {
+        "OBJECTID": 8,
+        "Name": "Palha Carga",
+        "Especie_Coral": null,
+        "AltMode": -1,
+        "Base": 0,
+        "Snippet": "",
+        "PopupInfo": "",
+        "HasLabel": -1,
+        "LabelID": 0
+      }
+    },
+    {
+      "type": "Feature",
+      "id": 9,
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -25.086466666999968,
+          16.82489444400005
+        ]
+      },
+      "properties": {
+        "OBJECTID": 9,
+        "Name": "Ponta do Farol",
+        "Especie_Coral": null,
+        "AltMode": -1,
+        "Base": 0,
+        "Snippet": "",
+        "PopupInfo": "",
+        "HasLabel": -1,
+        "LabelID": 0
+      }
+    },
+    {
+      "type": "Feature",
+      "id": 10,
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -25.06448611099995,
+          16.864222222000024
+        ]
+      },
+      "properties": {
+        "OBJECTID": 10,
+        "Name": "Baía de Fateja",
+        "Especie_Coral": null,
+        "AltMode": -1,
+        "Base": 0,
+        "Snippet": "",
+        "PopupInfo": "",
+        "HasLabel": -1,
+        "LabelID": 0
+      }
+    },
+    {
+      "type": "Feature",
+      "id": 11,
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -25.05224722199995,
+          16.877441667000028
+        ]
+      },
+      "properties": {
+        "OBJECTID": 11,
+        "Name": "Baía de Nho Jon",
+        "Especie_Coral": null,
+        "AltMode": -1,
+        "Base": 0,
+        "Snippet": "",
+        "PopupInfo": "",
+        "HasLabel": -1,
+        "LabelID": 0
+      }
+    }
+  ]
+}
